70553b7ab417761eca35b0afac4c1e623a813791
Wesley Garvin <garvinwes@gmail.com>
2023-07-03 17:12:51 -0400

    Initialize connection to MongoDB server
    - Install Mongoose dependency

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT;
+const url = process.env.MONGO_URL;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB server");
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
